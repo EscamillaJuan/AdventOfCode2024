f6938670a08cf69715889b70fa47a33869d036f8
EscamillaJuan <juanpedroescamilla@outlook.com>
2024-12-07 22:10:32 -0600

    day1 done (now in kotlin)

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,22 +1,41 @@
+import kotlin.math.absoluteValue
 
 
 fun main() {
     fun part1(input: List<String>): Int {
-        return input.size
+        val left = arrayListOf<Int>()
+        val right = arrayListOf<Int>()
+        var res = 0
+        input.map { line ->
+            val values = line.split("\\s{3}".toRegex())
+            left.add(values[0].toInt())
+            right.add(values[1].toInt())
+        }
+        left.sort()
+        right.sort()
+        left.zip(right).map { pair ->
+            res += (pair.first - pair.second).absoluteValue
+        }
+        return res
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        val left = arrayListOf<Int>()
+        val right = arrayListOf<Int>()
+        var res = 0
+        input.map { line ->
+            val values = line.split("\\s{3}".toRegex())
+            left.add(values[0].toInt())
+            right.add(values[1].toInt())
+        }
+        left.map{ location ->
+            val count = right.count{ it == location }
+            res += location * count
+        }
+
+        return res
     }
 
-    // Test if implementation meets criteria from the description, like:
-    check(part1(listOf("test_input")) == 1)
-
-    // Or read a large test input from the `src/input/day1.txt` file:
-    val testInput = readInput("day1")
-    check(part1(testInput) == 1)
-
-    // Read the input from the `src/input/Day01.txt` file.
     val input = readInput("day01")
     part1(input).println()
     part2(input).println()
